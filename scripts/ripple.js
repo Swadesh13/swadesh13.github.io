9a79bf74b1139ceffc967cf832ba808de06d6a6c
Swadesh13 <swadeshjana@gmail.com>
2022-05-29 10:57:28 +0530

    Major Update: Added projects section

--- scripts/ripple.js
+++ scripts/ripple.js
@@ -1,32 +1,32 @@
-$(".grid-item").click(function (e) {
-  // Remove any old one
-  $(".ripple").remove();
+$(".my-grid-item").click(function (e) {
+	// Remove any old one
+	$(".my-ripple").remove();
 
-  // Setup
-  var buttonWidth = $(this).width(),
-    buttonHeight = $(this).height();
+	// Setup
+	var buttonWidth = $(this).width(),
+		buttonHeight = $(this).height();
 
-  // Add the element
-  $(this).prepend("<span class='ripple'></span>");
+	// Add the element
+	$(this).prepend("<span class='my-ripple'></span>");
 
-  // Make it round!
-  if (buttonWidth >= buttonHeight) {
-    buttonHeight = buttonWidth;
-  } else {
-    buttonWidth = buttonHeight;
-  }
+	// Make it round!
+	if (buttonWidth >= buttonHeight) {
+		buttonHeight = buttonWidth;
+	} else {
+		buttonWidth = buttonHeight;
+	}
 
-  // Get the center of the element
-  var x = e.pageX - buttonWidth / 2;
-  var y = e.pageY - buttonHeight / 2;
+	// Get the center of the element
+	var x = e.pageX - buttonWidth / 2;
+	var y = e.pageY - buttonHeight / 2;
 
-  // Add the ripples CSS and start the animation
-  $(".ripple")
-    .css({
-      width: buttonWidth,
-      height: buttonHeight,
-      top: y + "px",
-      left: x + "px",
-    })
-    .addClass("rippleEffect");
+	// Add the my-ripples CSS and start the animation
+	$(".my-ripple")
+		.css({
+			width: buttonWidth,
+			height: buttonHeight,
+			top: y + "px",
+			left: x + "px",
+		})
+		.addClass("my-rippleEffect");
 });
